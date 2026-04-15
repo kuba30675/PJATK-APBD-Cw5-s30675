c514517ab6346c4d9c79c52db990e1675a344c57
Kuba <s30675@pjwstk.edu.pl>
2026-04-15 19:14:29 +0200

    Added Room DTOs

---
+++ DTOs/CreateRoomDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.DTOs;
+
+public class CreateRoomDto
+{
+    [MaxLength(30), Required]
+    public string Name { get; set; } = string.Empty;
+    [MaxLength(1), Required]
+    public char BuildingCode { get; set; }
+    [MaxLength(2)]
+    public int Floor { get; set; }
+    [MaxLength(2),Required]
+    public int Capacity { get; set; }
+    [Required]
+    public bool HasProjector { get; set; }
+}
---
+++ DTOs/RoomDto.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.DTOs;
+
+public class RoomDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public char BuildingCode { get; set; }
+    public int Floor { get; set; }
+    public int Capacity { get; set; }
+    public bool HasProjector { get; set; }
+}
