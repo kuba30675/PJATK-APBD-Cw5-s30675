e4f7d509597f0bd6e6d0ec2dbc1f3dfc8622bb19
Kuba <s30675@pjwstk.edu.pl>
2026-04-16 20:22:31 +0200

    Adjusted Data annotations in DTOs

--- DTOs/CreateRoomDto.cs
+++ DTOs/CreateRoomDto.cs
@@ -4,14 +4,9 @@ namespace WebApplication1.DTOs;
 
 public class CreateRoomDto
 {
-    [MaxLength(30), Required]
-    public string Name { get; set; } = string.Empty;
-    [Required]
-    public char BuildingCode { get; set; }
-    [Required]
-    public int Floor { get; set; }
-    [Required]
-    public int Capacity { get; set; }
-    [Required]
-    public bool HasProjector { get; set; }
+    [StringLength(30, MinimumLength = 3), Required] public string Name { get; set; } = string.Empty;
+    [Required] public char BuildingCode { get; set; }
+    [Required] public int Floor { get; set; }
+    [Required, Range(1, 35)] public int Capacity { get; set; }
+    [Required] public bool HasProjector { get; set; }
 }
--- DTOs/UpdateRoomDto.cs
+++ DTOs/UpdateRoomDto.cs
@@ -4,16 +4,11 @@ namespace WebApplication1.DTOs;
 
 public class UpdateRoomDto
 {
-    [MaxLength(30), Required]
-    public string Name { get; set; } = string.Empty;
-    [Required]
-    public char BuildingCode { get; set; }
-    [Required]
-    public int Floor { get; set; }
-    [Required]
-    public int Capacity { get; set; }
-    [Required]
-    public bool HasProjector { get; set; }
+    [StringLength(30, MinimumLength = 3), Required] public string Name { get; set; } = string.Empty;
+    [Required] public char BuildingCode { get; set; }
+    [Required] public int Floor { get; set; }
+    [Required, Range(1, 35)] public int Capacity { get; set; }
+    [Required] public bool HasProjector { get; set; }
 
     public bool IsActive { get; set; }
 }
