44b9f7ee0136c1893f2c575b657c437e03ca874f
Kuba <s30675@pjwstk.edu.pl>
2026-04-18 19:51:50 +0200

    Added ReservationController and all the needed DTOs for reservations

---
+++ Controllers/ReservationsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.DTOs;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ReservationsController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<IEnumerable<Reservation>> GetAll()
+    {
+ 
+    }
+    
+}
---
+++ DTOs/UpdateReservationDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.DTOs;
+
+public class UpdateReservationDto : IValidatableObject
+{
+    public int RoomId { get; set; }
+    [Required] public string OrganizerName { get; set; } = string.Empty;
+    [Required] public string Topic { get; set; } = string.Empty;
+    public DateOnly StartTime { get; set; }
+    public DateOnly EndTime { get; set; }
+    public string Status { get; set; } = string.Empty;
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (EndTime <= StartTime)
+        {
+            yield return new ValidationResult(
+                "End Time must be greater than start time",
+                new[] { nameof(EndTime) }
+            );
+        }
+    }
+}
